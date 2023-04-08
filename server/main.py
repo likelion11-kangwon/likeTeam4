1821f50b58f9b5ddbf2fb36a2154fc4faaaa46c3
Spicykong <ssw30951@gmail.com>
2023-04-08 14:58:26 +0900

    feat: 협업 폼 생성

--- server/main.py
+++ server/main.py
@@ -6,10 +6,33 @@ from Database import Database
 app = FastAPI()
 db = Database()
 
+# 조회수 가져옴
+def getViewsFromDb():
+    views = 0
+    return views
+
+# 조회수 + d 함
+def updateViews(d):
+    pass
+
+# 유저 아이피 반환
+def getIp():
+    userIp = ""
+    return userIp
+
+# 중복이면 참 반환, 처음이면 거짓
+def checkIp(ip):
+    ret = True
+    return ret
+
 @app.get("/views")
 def getViews():
-    return "hello"
+    return {"views":getViewsFromDb()}
 
 @app.post("/views")
 def getViews():
-    return "hello"
+    if checkIp(getIp()):
+        # 중복 요청 무시하고 403 반환
+        pass
+    updateViews()
+    #성공 이후 200 반환
