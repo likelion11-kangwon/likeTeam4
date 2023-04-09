6df2a3bc0fdf6b56534418ef0c047762426e77a6
nice-memory <doclecsj@naver.com>
2023-04-09 13:30:36 +0900

    feat: 조회수 증가 요청
    DB에서 ip 중복 여부를 판단하는 기능

--- server/main.py
+++ server/main.py
@@ -1,7 +1,6 @@
 from typing import Union
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI, Request, status, Response
 from Database import Database
-
 
 app = FastAPI()
 db = Database()
@@ -15,23 +14,24 @@ def getViewsFromDb():
 def updateViews(d):
     db.update("views", {"id":1, "count":int(getViewsFromDb())+d})
 
-# 유저 아이피 반환
-def getIp():
-    userIp = ""
-    return userIp
 
 # 중복이면 참 반환, 처음이면 거짓
 def checkIp(ip):
-    ret = True
-    return ret
+    if db.read("Address", {"ip":"'" + ip + "'"}) == []:
+        db.create("Address", {"ip":ip})
+        return False
+    else:
+        return True
 
+    
 @app.get("/views", status_code=200)
 def getViews():
     return {"views":getViewsFromDb()}
 
+
 @app.post("/views", status_code=201)
-def getViews(response: Response):
-    if checkIp(getIp()):
+def getViews(request: Request, response: Response):
+    if checkIp(request.client.host):
         response.status_code = 403
         return {"response":"중복된 요청"}
     updateViews(1)
